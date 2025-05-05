e078272b4f7caf4ecaf4f3a3696e4f1d9e50ce06
webtest25 <webtest25@proton.me>
2025-05-06 00:33:17 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+# A Simple Calculator app with functions
+
+def add(num1, num2):
+  return num1 + num2
+
+def sub(num1, num2):
+  return num1 - num2
+
+def div(num1, num2):
+  if num2 == 0:
+    return 'cannot dive by zero!'
+  else:
+    return num1 / num2
+
+def multi(num1, num2):
+  return num1 * num2
+
+# Take input
+n1 = int(input('Enter First Number: '))
+op = input(' Enter "+" or "-" or "*" or "/" only: ')
+n2 = int(input('Enter First Number: '))
+  
+if op == '+':
+  result = add(n1, n2)
+  
+elif op == '-':
+  result = sub(n1, n2)
+  
+elif op == '*':
+  result = multi(n1, n2)
+  
+elif op == '/':
+  result = div(n1, n2)
+else:
+  print('Enter correct op')
+print(f'The calculated result is: {result}')
